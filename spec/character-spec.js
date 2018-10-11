2f916937028fea1fbc126a76b700c794b24ba6b2
Hyung Lee and Kenneth Du <hyung@>
2018-10-11 10:01:57 -0700

    add initial files, add initial test

---
+++ spec/character-spec.js
@@ -0,0 +1,8 @@
+describe('Character', function() {
+
+  it('should properly construct a Character class', function() {
+    let Character = new Character("Chan", 100, 100, 100, 5)
+    expect(Chan.name).toEqual("Chan");
+  });
+    
+});
